6332801f4faba4063711d713d5056bd147a00dfa
Stefany1206 <125894822+Stefany1206@users.noreply.github.com>
2024-08-22 15:44:56 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,152 @@
+export const perguntas = [
+    {
+        enunciado: "Você chegou no último ano do ensino médio, e percebe que seu tempo de colegial está prestes a se findar. Qual é seu pensamento sobre isso?",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [
+                    "Está com medo do que está por vir.",
+                    "Achou assustador como o tempo passou rápido."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: [
+                    "Refletiu sobre sua dedicação e esforço, e está contente por estar encerrando um lindo ciclo de sua vida.",
+                    "Pensou em como boas escolhas levam à boas conquistas."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Decidir sobre qual carreira profisional seguir é um fato aterrorizante. Diante disso uma professora propôs um teste vocacional para os seus alunos. No que você acredita?",
+        alternativas: [
+            {
+                texto:  "Acredita que seja uma perca de tempo",
+                afirmacao: [
+                    "Sentiu-se seguro em deixar com que a vida o leve para o caminho que quiser.",
+                    "Não se importa nem um pouco sobre qual área seguir.",
+                
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Acredita ser uma boa ferramenta para trazer ideias e auxiliar sobre qual carreira seguir",
+                afirmacao: [
+                    "Com êxito respondeu todas as questões com facilidade.",
+                    "Sentiu-se confiante e pronto para fazer boas escolhas.",
+                    "Percebeu que seja qual for a carreira escolhida, será bem sucedido pois tem conviccção de um futuro promissor."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Ter um ponto de vista concretizado é algo que nos ajuda ter uma segurança sobre nossas escolhas. Pórem, você ainda se sente inseguro (a) sobre qual disciplina realmente estudar, o que você faz?",
+        alternativas: [
+            {
+                texto: "Faz pesquisas sobre muitas discilplinas, além de entender o que cada uma delas tem a oferecer",
+                afirmacao: [
+                    "Conseguiu ter bons resultados com suas pesquisas, e as mesmas lhe trouxeram um ótimo resultado.",
+                    "Percebeu que se informar sobre o que a graduação pode oferecer é de extrema importância, e lhe oferece grandes oportunidades.",
+                    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Prefere esperar e deixar para depois",
+                afirmacao: [
+                    "Não sente a necessidade de se informar sobre o que as universidades têm a oferecer.",
+                    "Achou entendiante.",
+                
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Conversar com familiares, amigos e profissionais é uma boa maneira de estruturar os seus objetivos. O que você faz?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que individualmente as coisas podem fluir melhor.",
+                afirmacao: [
+                    "Se sente desconfortável em compartilhar seus planos.",
+                    "Possui um temperamento de individualidade."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Se sente feliz em partilhar de uma nova experiência, podendo ter uma boa conversa com pessoas que lhe agregam",
+                afirmacao: [
+                    "Sente-se confiante em ter bons momentos de conversa.",
+                    "Acha necessário o ponto de vista de um bom profisional na área em que pretende atuar."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Algo que é muito afetado durante essa fase, é o nosso psicológico. Como você está se sentindo?",
+        alternativas: [
+            {
+                texto: "Sente-se preparado para os desafios que estão por vir.",
+                afirmacao: [
+                    "Reconhece seu potencial e também, sua maturidade para enfrentar as dificuldades que a vida nos traz.",
+                    "Possui um pensamento otimista sobre tudo à sua volta."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Sente-se perdido em tudo",
+                afirmacao: [
+                    "Sua preocupação com o futuro o paralisa, gerando medo e ansiedade.",
+                    "É pessimista e acredita não conseguir atingir seus objetivos."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Sonhar com o futuro é maravilhoso, mas, saber que logo estará vivendo uma vida adulta é intrigante. Como se sente?",
+        alternativas: [
+            {
+                texto: "Preocupado (a) com o fim de um ciclo, para começar algo totalmente novo",
+                afirmacao: [
+                    "Tem o costume de não conversar muito sobre os seus sonhos, pois lhe trazem inseguranças.",
+                    "Não gosta nem de imaginar como será essa mudança."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Alegre, pois sabe que independentemente do que irá acontecer, aproveitou cada segundo de sua adolescência, e está animado para iniciar uma nova etapa.",
+                afirmacao: [
+                    "Sempre foi alguém de corgagem.",
+                    "Acredita intensamente em seu potencial."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "É muito importante refletir sobre sua trajetória até aqui. Como foi? ",
+        alternativas: [
+            {
+                texto: "Não possui boas experiências para contar.",
+                afirmacao: [
+                    "Não se dedicou aos estudos e levou sua trajeória de forma irresponsável",
+                    "Não possui conhecimentos básicos para uma vida adulta.",
+                ],
+                    proxima: 7, 
+            },
+            {
+               texto: "Possui muitas experiências e conhecimentos válidos para repassar a sociedade.",
+                afirmacao: [
+                    "Sempre foi uma pessoa dedicada aos estudos.",
+                    "Seus esforços fizeram com que chegasse com um mérito admirável diante das pessoas à sua volta."
+            ],
+        },
+    ]
+}
+];
